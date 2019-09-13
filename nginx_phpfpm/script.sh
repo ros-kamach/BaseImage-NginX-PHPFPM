8195492d72011ea45f936dfe171989ceb97e956a
ros-kamach <47714061+ros-kamach@users.noreply.github.com>
2019-09-13 12:24:16 +0300

    Update script.sh

--- nginx_phpfpm/script.sh
+++ nginx_phpfpm/script.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 supervisord --nodaemon --configuration /etc/supervisor/conf.d/supervisord.conf
+FILE=/db_connect.sh
+if test -f "$FILE"; then
+    echo "$FILE exist"
+    bash /usr/share/nginx/db_connect.sh
+fi
